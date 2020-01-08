73b688fc0a0771492ff840531a84f8fc17702b60
mpostol <mpostol@cas.eu>
2020-01-08 14:45:01 +0100

    T4Definitions.csproj The project file could not be loaded #1
    - fixes #1

--- MAML/MAML.HelpTopics.Content/Properties/AssemblyInfo.cs
+++ MAML/MAML.HelpTopics.Content/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 ﻿//_______________________________________________________________
 //  Title   : Assembly info for: CAS.MAML.HelpTopics.Content
 //  System  : Microsoft VisualStudio 2015 / C#
-//  $LastChangedDate$
-//  $Rev$
-//  $LastChangedBy$
-//  $URL$
-//  $Id$
+//  $LastChangedDate:  $
+//  $Rev:  $
+//  $LastChangedBy: $
+//  $URL:  $
+//  $Id:  $
 //
-//  Copyright (c) 2000-2016 CAS LODZ POLAND
-//  +48 (42) 686 25 47
-//  techsupp@cas.eu
+//  Copyright (C) 2019, Mariusz Postol LODZ POLAND.
+//  +48 (608) 619 899
+//  mpostol@cas.eu
 //  www.cas.eu
 //_______________________________________________________________
 
@@ -19,9 +19,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CAS.MAML.HelpTopics.Content")]
 [assembly: AssemblyDescription("Library containing schema definition of the all topics xml file.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyCompany("Mariusz Postol")]
 [assembly: AssemblyProduct("CAS CommServer Family")]
-[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
+[assembly: AssemblyCopyright("Copyright (C) 2019, Mariusz Postol LODZ POLAND.")]
 [assembly: AssemblyTrademark("CommServer")]
 [assembly: AssemblyCulture("")]
 [assembly: Guid( "0d9f541e-f00b-4532-b4c3-e56bc0677916" )]
